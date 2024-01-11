e0b25f0bafd8af0bc16235c50bd603c0c9557b2d
jadihotsilalahi <153990513+jadihotsilalahi@users.noreply.github.com>
2024-01-11 18:22:00 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// script.js
+
+function Quiz1(url) {
+    window.location.href = 'https://forms.gle/48ZsaCrir2PYU3qRA';
+}
+
+function Quiz2(url) {
+    window.location.href = 'https://forms.gle/7vUvBtbvY8gsMoPFA';
+}
+
+function Quiz3(url) {
+    window.location.href = 'https://forms.gle/F9ZQY7sEiEZw6TxE8';
+}
